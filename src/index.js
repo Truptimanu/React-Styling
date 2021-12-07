f9fe3671be1b26177786bf929989f9d350df0ec1
Trupti <60674780+Truptimanu@users.noreply.github.com>
2021-12-07 15:36:45 -0500

    final styling in JSX.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true">
-      {" "}
       My favourite Foods.
     </h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <img className="food-img" alt="random img" src={img + "?grayscale"} />
   </div>,
   document.getElementById("root")
 );
