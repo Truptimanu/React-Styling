ea3d48fc3e78719db2f859a9abbf1e3d227fe396
Trupti <60674780+Truptimanu@users.noreply.github.com>
2021-12-07 16:00:10 -0500

    inline styling

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 const img = "https://picsum.photos/200";
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+customStyle.color = "blue";
+
 ReactDOM.render(
   <div>
-    <h1 className="heading" contentEditable="true">
+    <h1 style={customStyle} className="heading" contentEditable="true">
       My favourite Foods.
     </h1>
     <img className="food-img" alt="random img" src={img + "?grayscale"} />
